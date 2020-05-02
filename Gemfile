546cb202cb2faed7bdd96146611dcf4c4c90e820
Oscar Mier <voscarmv@gmail.com>
2020-05-02 17:58:22 -0500

    First tests with mysql2 gem.

--- Gemfile
+++ Gemfile
@@ -1,2 +1,5 @@
 source 'https://rubygems.org'
 gem 'rubocop'
+gem 'mysql2'
+gem 'nokogiri'
+gem 'httparty'
--- main.rb
+++ main.rb
@@ -1,1 +1,12 @@
-puts 'Hello, World!'
+require 'mysql2'
+require 'nokogiri'
+require 'httparty'
+
+client = Mysql2::Client.new(:host => "localhost", :username => "atlas", :database => 'world', :password => 'atlas')
+result = client.query('select countries.name as country, regions.name as province from countries join regions where countries.id = regions.country_id and countries.name = "Mexico";')
+
+result.each { |province|
+  puts "Coutry name: #{province['country']}"
+  puts "Province name: #{province['province']}"
+  puts "Wiki URL: https://en.wikipedia.org/wiki/2020_coronavirus_pandemic_in_#{province['country']}"
+}
